0b04e5f435d177cdadfeab9fdf410b26461edcdf
sorbon <clofsj@ya.ru>
2023-10-31 11:10:41 +0500

    create user

---
+++ app/Http/Requests/API/CreateUserRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\API;
+
+use App\Http\Responses\ApiResponse;
+use App\Rules\FindLoginRule;
+use App\Rules\IsEmailExistRule;
+use App\Rules\IsLoginExistRule;
+use App\Rules\IsRoleAllowedToAssignRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateUserRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'login' => [
+                'required',
+                'string',
+                new IsLoginExistRule()
+            ],
+            'email' => [
+                'required',
+                'email',
+                new IsEmailExistRule()
+            ],
+            'role' => [
+                'required',
+                'string',
+                new IsRoleAllowedToAssignRule()
+            ],
+            'password' => 'required|string',
+
+        ];
+    }
+
+    protected function failedValidation($validator)
+    {
+        $response = ApiResponse::warning($validator->getMessageBag());
+        throw new \Illuminate\Validation\ValidationException($validator, $response);
+    }
+
+}
