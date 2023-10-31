fffb430bdb4d8287974d5afae517a6384481b50b
sorbon <clofsj@ya.ru>
2023-10-31 12:19:37 +0500

    change routes

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
+Route::prefix('v1')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+    });
+    Route::middleware(\App\Http\Middleware\AuthenticateJWT::class)->group(function () {
+        Route::resource('user', \App\Http\Controllers\API\UserController::class);
+        Route::resource('client', \App\Http\Controllers\API\ClientController::class);
+        Route::resource('service', \App\Http\Controllers\API\ServiceController::class);
+        Route::resource('request', \App\Http\Controllers\API\RequestController::class);
+        Route::resource('request-status', \App\Http\Controllers\API\RequestStatusController::class);
+        Route::resource('comment', \App\Http\Controllers\API\CommentController::class);
+        Route::resource('phone', \App\Http\Controllers\API\PhoneController::class);
+    });
 });
-Route::middleware(\App\Http\Middleware\AuthenticateJWT::class)->group(function () {
-    Route::resource('user', \App\Http\Controllers\API\UserController::class);
-    Route::resource('client', \App\Http\Controllers\API\ClientController::class);
-    Route::resource('service', \App\Http\Controllers\API\ServiceController::class);
-    Route::resource('request', \App\Http\Controllers\API\RequestController::class);
-    Route::resource('request-status', \App\Http\Controllers\API\RequestStatusController::class);
-    Route::resource('comment', \App\Http\Controllers\API\CommentController::class);
-    Route::resource('phone', \App\Http\Controllers\API\PhoneController::class);
-});
-
