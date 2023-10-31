42f7854bb0ee1016b874a478946972981c4373cd
sorbon <clofsj@ya.ru>
2023-10-31 11:11:31 +0500

    change response

--- app/Http/Responses/ApiResponse.php
+++ app/Http/Responses/ApiResponse.php
@@ -4,6 +4,11 @@ namespace App\Http\Responses;
 
 namespace App\Http\Responses;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+
 class ApiResponse
 {
     public static function success($data, $message = null, $status = 200)
@@ -15,10 +20,25 @@ class ApiResponse
         ], $status);
     }
 
-    public static function error($message = null, $status = 400)
+    public static function error(Throwable $t = null, $message = null, $status = 400)
     {
+        $outputMessage = $message;
+        if ($t instanceof Throwable) {
+            if (empty($message)) {
+                $outputMessage = $t->getMessage();
+            }
+            Log::error($t);
+        }
         return response()->json([
             'status' => 'error',
+            'message' => $outputMessage
+        ], $status);
+    }
+
+    public static function warning($message = null, $status = 200)
+    {
+        return response()->json([
+            'status' => 'warning',
             'message' => $message
         ], $status);
     }
