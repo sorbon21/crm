c7a16458ae21684fcc36985cb2e602c18268203c
sorbon <clofsj@ya.ru>
2023-10-31 11:11:10 +0500

    seed for starting project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\RBAC;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +17,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        try {
+            DB::beginTransaction();
+            $adminRole = Role::findOrCreate(RBAC::Admin->value);
+            $operatorRole = Role::findOrCreate(RBAC::Operator->value);
+            $specialistRole = Role::findOrCreate(RBAC::Specialist->value);
+            Permission::findOrCreate('create user');
+            Permission::findOrCreate('show user');
+            Permission::findOrCreate('show users');
+            Permission::findOrCreate('assign role');
+            // Operator permissions
+            Permission::findOrCreate('create complaint');
+            Permission::findOrCreate('view complaints');
+            Permission::findOrCreate('search complaints');
+            // Specialist Back Office permissions
+            Permission::findOrCreate('create complaint');
+            Permission::findOrCreate('view complaints');
+            Permission::findOrCreate('edit complaint');
+            Permission::findOrCreate('add comment to complaint');
+            Permission::findOrCreate('search complaints');
+            $adminRole->syncPermissions(['create user', 'assign role', 'show user', 'show users']);
+            $operatorRole->syncPermissions(['create complaint', 'view complaints', 'search complaints']);
 
-        \App\Models\User::factory()->create([
-            'name' => 'Admin User',
-            'login' => 'admin',
-            'email' => 'admin@megafon.tj',
-            'password' => bcrypt('password'),
-        ]);
+            $specialistRole->syncPermissions([
+                'create complaint',
+                'view complaints',
+                'edit complaint',
+                'add comment to complaint',
+                'search complaints',
+            ]);
+            if (!User::where('login', 'admin')->first()) {
+                $user = User::factory()->create([
+                    'name' => 'Admin User',
+                    'login' => 'admin',
+                    'email' => 'admin@megafon.tj',
+                    'password' => bcrypt('password'),
+                ]);
+                $user->assignRole($adminRole);
+            }
+
+
+            DB::commit();
+
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+            DB::rollBack();
+        }
+        echo 'seed done';
+
+
     }
 }
